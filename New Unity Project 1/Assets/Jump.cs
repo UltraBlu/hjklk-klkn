8e7500565ee78d4dea929c4bcb1e6fe2c2ea2abf
UltraBlu <96628826+UltraBlu@users.noreply.github.com>
2021-12-24 16:22:51 +0000

    Add files via upload

---
+++ New Unity Project 1/Assets/Jump.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+{
+		   {
+			if (Input.GetKeyDown ("space")) {
+				GetComponent<Rigidbody> ().velocity = new Vector3 (0, 5, 0);
+		   }
+	}
+}
+}
---
+++ New Unity Project 1/Assets/ThirdPersonCameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCameraController : MonoBehaviour 
+	{
+
+	public float RotationSpeed = 10;
+	public Transform gun, Player;
+	float mouseX, mouseY;
+
+
+	void Start (){
+
+	}
+	
+	void LateUpdate ()
+	{
+		CamControl ();
+	}
+
+	void CamControl ()
+	{
+		mouseX += Input.GetAxis ("Mouse X") * RotationSpeed;
+		mouseY -= Input.GetAxis ("Mouse Y") * RotationSpeed;
+		mouseY = Mathf.Clamp (mouseY, -35, 60);
+
+
+		transform.LookAt (gun);
+
+
+		gun.rotation = Quaternion.Euler (mouseY, mouseX, 0);
+		Player.rotation = Quaternion.Euler (0, mouseX, 0);
+	}
+}
